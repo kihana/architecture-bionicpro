78a985d0d46486b3c2d180b27a5ecab450aa5dfa
Egor Zayats <yahor.zayats@atotech.com>
2025-05-03 11:37:54 +0200

    Added token validation for task2.

--- api/main.py
+++ api/main.py
@@ -1,18 +1,23 @@
-
-from fastapi import FastAPI
+import os
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import jwt
+from jwt import PyJWKClient
 from pydantic import BaseModel
 from typing import List
 from datetime import datetime
 import random
 import uuid
+import requests
+
+security = HTTPBearer()
 
 app = FastAPI()
 
-# Разрешаем CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -25,8 +30,30 @@ class Report(BaseModel):
     status: str
     value: float
 
+KEYCLOAK_URL = "http://keycloak:8080"
+KEYCLOAK_REALM = "reports-realm"
+KEYCLOAK_CERTS_URL = KEYCLOAK_URL + "/realms/" + KEYCLOAK_REALM + "/protocol/openid-connect/certs"
+ALGORITHM = "RS256"
+REQUIRED_ROLE = "prothetic_user"
+
 @app.get("/reports", response_model=List[Report])
-def get_reports():
+def get_reports(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    try:
+        jwks_client = PyJWKClient(KEYCLOAK_CERTS_URL)
+        signing_key = jwks_client.get_signing_key_from_jwt(token)
+        payload = jwt.decode(token, signing_key.key, algorithms=[ALGORITHM])
+        roles: List[str] = payload.get("realm_access", {}).get("roles", [])
+        if REQUIRED_ROLE not in roles:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Access denied"
+                )
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Access token expired")
+    except jwt.InvalidTokenError as e:
+        raise HTTPException(status_code=401, detail=f"Invalid token: {str(e)}")
+
     statuses = ["pending", "completed", "failed"]
     reports = [
         Report(
